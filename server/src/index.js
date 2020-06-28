7884f4623041cab6b4a4b7c2506ddd06109adf1c
unknown <hyunsu.lee@navercorp.com>
2020-06-28 20:03:51 +0900

    [server] server init #3

---
+++ server/src/index.js
@@ -0,0 +1,19 @@
+const Koa = require("koa");
+const Router = require("koa-router");
+
+const api = require("./routes");
+
+const app = new Koa();
+const router = new Router();
+
+// 라우터 모듈 추가
+router.use("/api", api.routes());
+
+// 라우터 미들웨어 추가
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+// 서버 포트 설정
+app.listen(4200, () => {
+  console.log("Server is Running PORT: 4200");
+});
