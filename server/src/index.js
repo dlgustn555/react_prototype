358fab40649d44b16d9a97d7ea47c588b34e72f3
dlgustn555 <dlgustn555@naver.com>
2020-06-29 16:56:24 +0900

    WebHooks 추가 #2

--- server/src/index.js
+++ server/src/index.js
@@ -1,19 +1,28 @@
-const Koa = require("koa");
-const Router = require("koa-router");
+const Koa = require('koa')
+const Router = require('koa-router')
+const bodyParser = require('koa-bodyparser')
 
-const api = require("./routes");
+const api = require('./routes')
 
-const app = new Koa();
-const router = new Router();
+const app = new Koa()
+const router = new Router()
+
+// 웹훅 추가
+router.post('/payload', (ctx) => {
+    ctx.body = ctx.body
+})
 
 // 라우터 모듈 추가
-router.use("/api", api);
+router.use('/api', api)
+
+// bodyParser는 라우터 코드보다 상단에 있어야 함.
+app.use(bodyParser())
 
 // 라우터 미들웨어 추가
-app.use(router.routes());
-app.use(router.allowedMethods());
+app.use(router.routes())
+app.use(router.allowedMethods())
 
 // 서버 포트 설정
 app.listen(4200, () => {
-  console.log("Server is Running PORT: 4200");
-});
+    console.log('Server is Running PORT: 4200')
+})
